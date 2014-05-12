2fa6ff12e63b739b9519ad51c61dc4a1d064c5ea
Dmitry Davydov <haudvd@gmail.com>
2014-05-13 00:50:53 +0400

    remove old lines

--- app.js
+++ app.js
@@ -28,12 +28,5 @@ var routes = require('./routes')
 routes.init(app, io);
 polyfills.init();
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-
 module.exports.app = app
 module.exports.io = io
